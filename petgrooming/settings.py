54cea9702d5f12bb54eddc7d6d4aab8d10bde480
sibuj607-commits <yournewemail@example.com>
2026-08-05 13:04:03 +0530

    Added Cloudinary image upload

--- petgrooming/settings.py
+++ petgrooming/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import dj_database_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -91,6 +93,12 @@ DATABASES = {
     }
 }
 
+
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL")
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
